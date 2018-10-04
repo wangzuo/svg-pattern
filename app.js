b3e61ae9a170121fcc3e01f9e8e9a99ba5a33834
Wang Zuo <wzuoadjusted@gmail.com>
2018-10-05 00:08:43 +0800

    body styling

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import './global.scss';
 import styles from './app.module.scss';
+import cx from 'classnames';
 import React, { Component } from 'react';
+import Body from './body';
 
 export default class App extends Component {
   state = {
-    svg: ''
+    tab: 0,
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="199" viewBox="0 0 100 199"><g fill="#000"><path d="M0 199V0h1v1.99L100 199h-1.12L1 4.22V199H0zM100 2h-.12l-1-2H100v2z"></path></g></svg>`
   };
 
   handleSvgChange = e => {
@@ -11,14 +15,56 @@ export default class App extends Component {
   };
 
   render() {
+    const bodyStyle = {
+      backgroundImage: `url("data:image/svg+xml,${encodeURIComponent(
+        this.state.svg
+      )}")`
+    };
+
     return (
-      <div className={styles.container}>
-        <textarea onChange={this.handleSvgChange} />
-        <div
-          style={{ backgroundImage: url(encodeURIComponent(this.state.svg)) }}>
-          hello world
+      <Body style={bodyStyle}>
+        <div className={styles.container}>
+          <div className={styles.tabs}>
+            <div
+              className={cx(styles.tab, {
+                [styles.tabActive]: this.state.tab === 0
+              })}
+              onClick={() => this.setState({ tab: 0 })}
+            >
+              svg
+            </div>
+            <div
+              className={cx(styles.tab, {
+                [styles.tabActive]: this.state.tab === 1
+              })}
+              onClick={() => this.setState({ tab: 1 })}
+            >
+              css
+            </div>
+          </div>
+
+          {this.state.tab === 0 && (
+            <textarea
+              className={styles.textarea}
+              onChange={this.handleSvgChange}
+              value={this.state.svg}
+              rows="10"
+              placeholder="svg"
+            />
+          )}
+
+          {this.state.tab === 1 && (
+            <textarea
+              className={styles.textarea}
+              value={`background-image: ${bodyStyle.backgroundImage}`}
+              rows="10"
+              placeholder="svg"
+            />
+          )}
+
+          <a href="https://github.com/wangzuo/svg-pattern">Source</a>
         </div>
-      </div>
+      </Body>
     );
   }
 }
---
+++ body.js
@@ -0,0 +1,24 @@
+import { Component, Children } from 'react';
+import withSideEffect from 'react-side-effect';
+
+class Body extends Component {
+  render() {
+    return Children.only(this.props.children);
+  }
+}
+
+function reducePropsToState(propsList) {
+  const style = {};
+  propsList.forEach(function(props) {
+    Object.assign(style, props.style);
+  });
+  return style;
+}
+
+function handleStateChangeOnClient(style) {
+  Object.assign(document.body.style, style);
+}
+
+export default withSideEffect(reducePropsToState, handleStateChangeOnClient)(
+  Body
+);
